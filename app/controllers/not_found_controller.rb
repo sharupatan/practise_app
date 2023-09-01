8093788f025fc4c9f89b81cd2f65ae95cfbdf193
sharupatan <patansharu654321@gmail.com>
2023-09-01 20:46:07 +0530

    sending file by creating pdf using prawn

--- app/controllers/not_found_controller.rb
+++ app/controllers/not_found_controller.rb
@@ -1,4 +1,17 @@
+require 'prawn'
+
 class NotFoundController < ApplicationController
 	def index
+		send_data generate_pdf,filename: 'hello.pdf', type: 'application/pdf'
+	end
+
+	private
+
+	def generate_pdf
+		Prawn::Document.new do
+			text 'sharu', align: :center
+			text "Address:"
+			text "Email:"
+		end.render
 	end
 end
